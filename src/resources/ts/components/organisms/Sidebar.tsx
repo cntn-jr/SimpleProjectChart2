e3bc3e93919d351d3d267378ad861a8fdd2c1c61
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-19 14:34:21 +0900

    add: Changed the sidebar to show which pages are displayed
    ・サイドバーにオリジナルのコンポーネントを作成し、表示しているページがわかるように変更しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,6 +1,13 @@
-import { MenuItem, MenuList, Stack } from "@mui/material";
-import { useNavigate } from "react-router";
+import { MenuItem, MenuItemProps, MenuList, Stack } from "@mui/material";
+import { blue, grey } from "@mui/material/colors";
+import { styled } from "@mui/system";
+import { useMatch, useNavigate } from "react-router";
 import { MainRouters } from "../../router/MainRouters";
+
+const SidebarMenuItem = styled(MenuItem)<MenuItemProps>(({ selected }) => ({
+    height: "80px",
+    color: selected ? blue[300] : grey[800],
+}));
 
 export const Sidebar = () => {
     const authentication = true;
@@ -8,18 +15,26 @@ export const Sidebar = () => {
     const onClickMenu = (path: string) => {
         navigate(path);
     };
+    const isSelectRoute = (path: string) => {
+        if (useMatch(path)) {
+            return true;
+        }
+        return false;
+    };
+
     return (
         <Stack direction="row" mt="10px">
             <MenuList sx={{ width: "100%" }}>
                 {Object.values(MainRouters).map((route) =>
                     route.authenticate === authentication ? (
-                        <MenuItem
+                        <SidebarMenuItem
                             onClick={() => onClickMenu(route.path)}
                             key={route.path}
                             sx={{ height: "80px" }}
+                            selected={isSelectRoute(route.path)}
                         >
                             {route.name}
-                        </MenuItem>
+                        </SidebarMenuItem>
                     ) : null
                 )}
             </MenuList>
