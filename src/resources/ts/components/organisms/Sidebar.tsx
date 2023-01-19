adcb9b98962c62c941149ea335a7fb594a9be9e2
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-19 14:27:20 +0900

    add: Click to Page function
    ・クリックするとページ遷移する機能を作成しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,15 +1,23 @@
 import { MenuItem, MenuList, Stack } from "@mui/material";
+import { useNavigate } from "react-router";
 import { MainRouters } from "../../router/MainRouters";
 
 export const Sidebar = () => {
     const authentication = true;
-
+    const navigate = useNavigate();
+    const onClickMenu = (path: string) => {
+        navigate(path);
+    };
     return (
         <Stack direction="row" mt="10px">
             <MenuList sx={{ width: "100%" }}>
                 {Object.values(MainRouters).map((route) =>
                     route.authenticate === authentication ? (
-                        <MenuItem key={route.path} sx={{ height: "80px" }}>
+                        <MenuItem
+                            onClick={() => onClickMenu(route.path)}
+                            key={route.path}
+                            sx={{ height: "80px" }}
+                        >
                             {route.name}
                         </MenuItem>
                     ) : null
