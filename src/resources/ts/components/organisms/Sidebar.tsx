11484b622161de88c31710e53086506ab09b2a22
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-19 14:09:09 +0900

    add: Created the basis for the sidebar
    ・サイドバーの基礎となるものを作成しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,4 +1,20 @@
+import { MenuItem, MenuList, Stack } from "@mui/material";
+import { MainRouters } from "../../router/MainRouters";
 
 export const Sidebar = () => {
-    return <></>;
+    const authentication = true;
+
+    return (
+        <Stack direction="row" mt="10px">
+            <MenuList sx={{ width: "100%" }}>
+                {Object.values(MainRouters).map((route) =>
+                    route.authenticate === authentication ? (
+                        <MenuItem key={route.path} sx={{ height: "80px" }}>
+                            {route.name}
+                        </MenuItem>
+                    ) : null
+                )}
+            </MenuList>
+        </Stack>
+    );
 };
