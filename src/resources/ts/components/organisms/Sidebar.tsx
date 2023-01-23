995eac4da2c35077c9e5bdf2e30b1bd1eb1dd1b6
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-23 10:05:53 +0900

    add: menu icons
    ・サイバーのメニューにアイコンを挿入しました。

--- src/resources/ts/components/organisms/Sidebar.tsx
+++ src/resources/ts/components/organisms/Sidebar.tsx
@@ -1,13 +1,31 @@
-import { MenuItem, MenuItemProps, MenuList, Stack } from "@mui/material";
-import { blue, grey } from "@mui/material/colors";
-import { styled } from "@mui/system";
+import {
+    createTheme,
+    ListItemIcon,
+    ListItemText,
+    MenuItem,
+    MenuList,
+    Stack,
+    ThemeProvider,
+} from "@mui/material";
+import { grey } from "@mui/material/colors";
 import { useMatch, useNavigate } from "react-router";
 import { MainRouters } from "../../router/MainRouters";
 
-const SidebarMenuItem = styled(MenuItem)<MenuItemProps>(({ selected }) => ({
-    height: "80px",
-    color: selected ? blue[300] : grey[800],
-}));
+const sidebarTheme = createTheme({
+    components: {
+        MuiMenuItem: {
+            styleOverrides: {
+                root: {
+                    height: "80px",
+                    color: grey[600],
+                },
+                selected: {
+                    backgroundColor: grey[200],
+                },
+            },
+        },
+    },
+});
 
 export const Sidebar = () => {
     const authentication = true;
@@ -23,21 +41,23 @@ export const Sidebar = () => {
     };
 
     return (
-        <Stack direction="row" mt="10px">
-            <MenuList sx={{ width: "100%" }}>
-                {Object.values(MainRouters).map((route) =>
-                    route.authenticate === authentication ? (
-                        <SidebarMenuItem
-                            onClick={() => onClickMenu(route.path)}
-                            key={route.path}
-                            sx={{ height: "80px" }}
-                            selected={isSelectRoute(route.path)}
-                        >
-                            {route.name}
-                        </SidebarMenuItem>
-                    ) : null
-                )}
-            </MenuList>
-        </Stack>
+        <ThemeProvider theme={sidebarTheme}>
+            <Stack direction="row" mt="10px">
+                <MenuList sx={{ width: "100%" }}>
+                    {Object.values(MainRouters).map((route) =>
+                        route.authenticate === authentication ? (
+                            <MenuItem
+                                onClick={() => onClickMenu(route.path)}
+                                key={route.path}
+                                selected={isSelectRoute(route.path)}
+                            >
+                                <ListItemIcon>{route.icon}</ListItemIcon>
+                                <ListItemText primary={route.name} />
+                            </MenuItem>
+                        ) : null
+                    )}
+                </MenuList>
+            </Stack>
+        </ThemeProvider>
     );
 };
