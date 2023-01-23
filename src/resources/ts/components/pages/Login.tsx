5c575fa6c5200501d5257dba01eae96a7bc2d123
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-23 13:53:40 +0900

    add: login form
    ・ログインページのフォームを作成しました。

--- src/resources/ts/components/pages/Login.tsx
+++ src/resources/ts/components/pages/Login.tsx
@@ -1,5 +1,47 @@
-import { Typography } from "@mui/material";
+import LoginIcon from "@mui/icons-material/Login";
+import {
+    Box,
+    Button,
+    TextField,
+} from "@mui/material";
+import { blue } from "@mui/material/colors";
 
 export const Login = () => {
-    return (<Typography component="h1" color="cyan"></Typography>);
+    return (
+        <Box
+            component="form"
+            sx={{
+                width: "60%",
+                backgroundColor: "#fff",
+                mt: "40px",
+                mx: "auto",
+                padding: "10px",
+                borderRadius: "5px",
+            }}
+        >
+            <TextField
+                type="email"
+                label="Email"
+                fullWidth
+                required
+                margin="normal"
+            />
+            <TextField
+                type="password"
+                label="Password"
+                fullWidth
+                required
+                margin="normal"
+            />
+            <Button
+                type="button"
+                fullWidth
+                variant="contained"
+                startIcon={<LoginIcon />}
+                sx={{ mt: "15px", mb: "10px", backgroundColor: blue[600] }}
+            >
+                LOG IN
+            </Button>
+        </Box>
+    );
 };
