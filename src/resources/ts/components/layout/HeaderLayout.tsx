c83b9f564faedd8ea051fd930a7b97d6ab3280a5
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-23 10:19:50 +0900

    add: subtitle
    ・サブタイトルとそのスタイルを作成しました。

--- src/resources/ts/components/layout/HeaderLayout.tsx
+++ src/resources/ts/components/layout/HeaderLayout.tsx
@@ -1,17 +1,30 @@
 import { AppBar, Box, Typography } from "@mui/material";
+import { useState } from "react";
+import { useLocation } from "react-router";
 
 export const HeaderLayout = () => {
+    const [subTitle, setSubTitle] = useState("SubTitle");
     return (
         <AppBar position="absolute">
-            <Box height="60px">
+            <Box height="60px" display="flex">
                 <Typography
                     component="h1"
                     variant="h5"
                     pt="20px"
                     pb="10px"
-                    pl="20px"
+                    pl="80px"
+                    fontWeight="bold"
                 >
                     ProjectA
+                </Typography>
+                <Typography
+                    component="h3"
+                    variant="h6"
+                    pt="20px"
+                    pb="10px"
+                    mx="auto"
+                >
+                    {subTitle}
                 </Typography>
             </Box>
         </AppBar>
