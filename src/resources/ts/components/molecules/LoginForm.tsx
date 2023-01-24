6dd4fc3e52cc13a246f9de6d69f92b27d5e3e714
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-24 09:33:43 +0900

    update: Check the behavior of userLoginForm
    ・userLoginFormの関数の挙動を確認しました。

--- src/resources/ts/components/molecules/LoginForm.tsx
+++ src/resources/ts/components/molecules/LoginForm.tsx
@@ -1,9 +1,13 @@
 import LoginIcon from "@mui/icons-material/Login";
 import { TextField } from "@mui/material";
+import { useLoginForm } from "../../hooks/User/useLoginForm";
+import { showAtom } from "../../util/showAtom";
 import { FormCard } from "../atoms/FormCard";
 import { FullWidthButton } from "../atoms/FullWidthButton";
 
 export const LoginForm = () => {
+    const { changeEmail, changePassword } = useLoginForm();
+    const { showUser } = showAtom();
     return (
         <FormCard>
             <TextField
@@ -12,6 +16,7 @@ export const LoginForm = () => {
                 fullWidth
                 required
                 margin="normal"
+                onChange={changeEmail}
             />
             <TextField
                 type="password"
@@ -19,8 +24,9 @@ export const LoginForm = () => {
                 fullWidth
                 required
                 margin="normal"
+                onChange={changePassword}
             />
-            <FullWidthButton startIcon={<LoginIcon />} onClick={() => {}}>
+            <FullWidthButton startIcon={<LoginIcon />} onClick={showUser}>
                 LOG IN
             </FullWidthButton>
         </FormCard>
