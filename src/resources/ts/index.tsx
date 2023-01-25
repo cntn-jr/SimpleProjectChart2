a0fe9ed1a4b6f8b1bb184cb08d5e6e0427d3a651
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-25 10:26:03 +0900

    add: Introduction of react-query

--- src/resources/ts/index.tsx
+++ src/resources/ts/index.tsx
@@ -1,6 +1,20 @@
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+// react-query全体での初期設定
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            suspense: true,
+        },
+    },
+});
 
 const container = document.getElementById("app");
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<App />);
+root.render(
+    <QueryClientProvider client={queryClient}>
+        <App />
+    </QueryClientProvider>
+);
