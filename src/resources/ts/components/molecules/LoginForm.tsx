de2e8879babfdd0b4ef57df119a0275a2eb10ad1
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-27 14:26:43 +0900

    add: login button is disable if two Textfield is empty on login form
    ・ログインフォームにてTextFieldが空の場合、ログインボタンを押せないように変更しました。

--- src/resources/ts/components/molecules/LoginForm.tsx
+++ src/resources/ts/components/molecules/LoginForm.tsx
@@ -1,9 +1,11 @@
 import LoginIcon from "@mui/icons-material/Login";
-import { Alert, Snackbar, TextField } from "@mui/material";
+import { TextField } from "@mui/material";
+import { useMemo } from "react";
 import { useRecoilValue } from "recoil";
 import { useLoginForm } from "../../hooks/User/useLoginForm";
 import { errorAtom } from "../../recoil/errorAtom";
 import { loginLoadingAtom } from "../../recoil/loginLoadingAtom";
+import { userAtom } from "../../recoil/userAtom";
 import { ErrorAlert } from "../atoms/ErrorAlert";
 import { FormCard } from "../atoms/FormCard";
 import { FullWidthButton } from "../atoms/FullWidthButton";
@@ -11,6 +13,11 @@ import { FullWidthButton } from "../atoms/FullWidthButton";
 export const LoginForm = () => {
     const loading = useRecoilValue(loginLoadingAtom);
     const error = useRecoilValue(errorAtom);
+    const user = useRecoilValue(userAtom);
+    const disabled = useMemo(() => {
+        if (user.email && user.password) return false;
+        return true;
+    }, [user]);
     const { changeEmail, changePassword, onClickLogin } = useLoginForm();
     return (
         <FormCard>
@@ -39,6 +46,7 @@ export const LoginForm = () => {
                 startIcon={<LoginIcon />}
                 loading={loading}
                 onClick={onClickLogin}
+                disabled={disabled}
             >
                 LOG IN
             </FullWidthButton>
