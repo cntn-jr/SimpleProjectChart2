dfb26939ef2c1f3a07502d9bcf02d9c2b5618d4a
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-27 15:03:33 +0900

    add: create FullDisplaySpinner
    ・アプリを操作されたくないときに表示するスピナーを作成しました。

--- src/resources/ts/App.tsx
+++ src/resources/ts/App.tsx
@@ -1,12 +1,14 @@
 import { BrowserRouter } from "react-router-dom";
 import { RecoilRoot } from "recoil";
 import { BasicToaster } from "./components/atoms/BasicToaster";
+import { FullDisplaySpinner } from "./components/atoms/FullDisplaySpinner";
 import { AppLayout } from "./components/layout/AppLayout";
 
 export default function App() {
     return (
         <RecoilRoot>
             <BasicToaster />
+            <FullDisplaySpinner />
             <BrowserRouter>
                 <AppLayout />
             </BrowserRouter>
---
+++ src/resources/ts/recoil/isBackdropAtom.ts
@@ -0,0 +1,6 @@
+import { atom } from "recoil";
+
+export const isBackdropAtom = atom({
+    key: "isBackdropAtom",
+    default: <boolean>false,
+});
