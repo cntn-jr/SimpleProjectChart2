7c29ab489fc09304950f09a6749684e1e9d31c03
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-27 12:09:53 +0900

    update: open and close of BasicToast function
    ・BasicToastに表示と非表示にする機能を追加しました。

--- src/resources/ts/components/atoms/BasicToaster.tsx
+++ src/resources/ts/components/atoms/BasicToaster.tsx
@@ -1,18 +1,24 @@
 import { Alert, AlertColor, Snackbar } from "@mui/material";
+import { useRecoilState } from "recoil";
+import { toastAtom } from "../../recoil/toastAtom";
 
 type Props = {
-    open: boolean;
     severity: AlertColor;
     message: string;
 };
 
 export const BasicToaster = (props: Props) => {
-    const { open, severity, message } = props;
+    const { severity, message } = props;
+    const [open, setOpen] = useRecoilState(toastAtom);
+    const onClose = () => {
+        setOpen(false);
+    };
     return (
         <Snackbar
             open={open}
             autoHideDuration={10000}
             anchorOrigin={{ vertical: "top", horizontal: "right" }}
+            onClose={onClose}
         >
             <Alert severity={severity}>{message}</Alert>
         </Snackbar>
