385e4c6df97101a76de1faaead960b2042be680b
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-27 23:46:22 +0900

    add: create contents on Ganttchart model
    ・GanttChartに記述すべき設定とメソッドを追加しました。

--- src/app/Models/Ganttchart.php
+++ src/app/Models/Ganttchart.php
@@ -4,8 +4,23 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Ganttchart extends Model
 {
     use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'start',
+        'end',
+        'user_id',
+    ];
+
+    public function getGantt($user_id)
+    {
+        $charts = $this->select(['id', 'name', 'start', 'end', 'type', 'progress'])->where('user_id', $user_id)->get();
+        return $charts;
+    }
 }
