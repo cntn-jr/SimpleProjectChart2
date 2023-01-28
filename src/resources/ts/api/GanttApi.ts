c2f621f49b0cd634ba4b251bca58d23a04daf786
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-28 15:46:47 +0900

    update: move start and end being to date from string to GanttApi.ts
    ・開始と終了を文字列から日付にする機能を GanttApi.ts へ移動させた

--- src/resources/ts/api/GanttApi.ts
+++ src/resources/ts/api/GanttApi.ts
@@ -4,7 +4,17 @@ import { Task } from "gantt-task-react";
 export const GanttApi = () => {
     const getGantt = async () => {
         const { data } = await axios.get<Array<Task>>("api/ganttchart/get");
-        return data;
+        // startとendをDate型に変換する
+        let newTasks: Task[] = [];
+        data!.forEach((task) => {
+            let newTask = {
+                ...task,
+                start: new Date(task.start),
+                end: new Date(task.end),
+            };
+            newTasks.push(newTask);
+        });
+        return newTasks;
     };
     return { getGantt };
 };
--- src/resources/ts/components/pages/GanttChart.tsx
+++ src/resources/ts/components/pages/GanttChart.tsx
@@ -8,38 +8,15 @@ import {
     DisplayOption,
 } from "gantt-task-react";
 import "gantt-task-react/dist/index.css";
-import { useEffect, useState } from "react";
 import { useGantt } from "../../hooks/Gantt/useGantt";
 
 export const GanttChart = () => {
     const { getGanttQuery } = useGantt();
     const { data } = getGanttQuery();
-    let [tasks, setTasks] = useState<Array<Task>>([
-        {
-            type: "task",
-            start: new Date(2020, 1, 1),
-            end: new Date(2020, 1, 2),
-            name: "Idea",
-            id: "Task 1",
-            progress: 100,
-        },
-    ]);
-    useEffect(() => {
-        let newTasks: Task[] = [];
-        data!.forEach((task) => {
-            let newTask = {
-                ...task,
-                start: new Date(task.start),
-                end: new Date(task.end),
-            };
-            newTasks.push(newTask);
-        });
-        setTasks([...newTasks]);
-    }, []);
 
     return (
         <Gantt
-            tasks={tasks}
+            tasks={data!}
             listCellWidth=""
             ganttHeight={500}
             // viewDate={displayDate}
