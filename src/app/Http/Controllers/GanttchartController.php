f26e00a5df2f57f75b3f90898e04e44b06fb33da
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-01-30 11:50:41 +0900

    add: create gantt data api and this test
    ・ガントチャートの予定を作成するAPIとそのテストを作成しました。

--- src/app/Http/Controllers/GanttchartController.php
+++ src/app/Http/Controllers/GanttchartController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Models\Ganttchart;
+use Exception;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
 
 class GanttchartController extends Controller
 {
@@ -13,5 +15,33 @@ class GanttchartController extends Controller
         $ganttchart_model = new Ganttchart();
         $charts = $ganttchart_model->getGantt(Auth::user()->id);
         return response()->json($charts);
+    }
+
+    public function store(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'name' => ['required', 'string', 'max:63',],
+            'start' => ['required', 'date',],
+            'end' => ['required', 'date', 'after_or_equal:start',]
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            $response['errors'] = ['name' => [], 'start' => [], 'end' => [],];
+            foreach ($errors as $error_key => $error) {
+                $response['errors'][$error_key] = $error;
+            };
+            return response()->json($response, 401);
+        }
+        try {
+            $schedule = new Ganttchart();
+            $schedule->name = $request->name;
+            $schedule->start = $request->start;
+            $schedule->end = $request->end;
+            $schedule->user_id = Auth::user()->id;
+            $schedule->save();
+        } catch (Exception $err) {
+            return response()->json([], 401);
+        };
     }
 }
