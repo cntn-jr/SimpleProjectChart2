af7b51d4c3c6d129ee416d864d919e00d8046780
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-02 10:20:46 +0900

    add: update schedule api and this api
    ・予定を更新するAPIとそのテストを作成しました。

--- src/app/Http/Controllers/GanttchartController.php
+++ src/app/Http/Controllers/GanttchartController.php
@@ -8,6 +8,8 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
 
+use function PHPUnit\Framework\throwException;
+
 class GanttchartController extends Controller
 {
     public function index()
@@ -39,6 +41,36 @@ class GanttchartController extends Controller
             $schedule->start = $request->start;
             $schedule->end = $request->end;
             $schedule->user_id = Auth::user()->id;
+            $schedule->save();
+        } catch (Exception $err) {
+            return response()->json([], 401);
+        };
+    }
+
+    public function update(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'id' => ['required', 'exists:App\Models\Ganttchart,id',],
+            'name' => ['required', 'string', 'max:63',],
+            'start' => ['required', 'date',],
+            'end' => ['required', 'date', 'after_or_equal:start',]
+        ]);
+
+        if ($validator->fails()) {
+            $errors = $validator->errors()->toArray();
+            $response['errors'] = ['id' => [], 'name' => [], 'start' => [], 'end' => [],];
+            foreach ($errors as $error_key => $error) {
+                $response['errors'][$error_key] = $error;
+            };
+            return response()->json($response, 401);
+        }
+        try {
+            $schedule = Ganttchart::find($request->id);
+            if (Auth::user()->id != $schedule->user_id)
+                throw new Exception();
+            $schedule->name = $request->name;
+            $schedule->start = $request->start;
+            $schedule->end = $request->end;
             $schedule->save();
         } catch (Exception $err) {
             return response()->json([], 401);
