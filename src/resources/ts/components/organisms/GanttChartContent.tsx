bdd016f8301e35c004b6cd6436a375952b67f216
cntn-jr <65937810+cntn-jr@users.noreply.github.com>
2023-02-04 14:46:36 +0900

    create: the buttons has been created to toggle the display of Gantt charts.
    ・ガントチャートの表示を切り替えるボタン群を作成しました

--- src/resources/ts/components/organisms/GanttChartContent.tsx
+++ src/resources/ts/components/organisms/GanttChartContent.tsx
@@ -1,4 +1,10 @@
-import { Box, Typography } from "@mui/material";
+import {
+    Box,
+    FormControlLabel,
+    Radio,
+    RadioGroup,
+    Typography,
+} from "@mui/material";
 import { blue, grey } from "@mui/material/colors";
 import {
     Gantt,
@@ -17,6 +23,9 @@ import { GanttAddModal } from "../molecules/GanttAddModal";
 import { GanttUpdateModal } from "../molecules/GanttUpdateModal";
 
 export const GanttChartContent = () => {
+    const [view, setView] = useState<
+        ViewMode.Day | ViewMode.Month | ViewMode.Year
+    >(ViewMode.Day);
     const [open, setOpen] = useState<boolean>(false);
     const [openSchedule, setOpenSchedule] = useState<boolean>(false);
     const [schedule, setSchedule] = useRecoilState(scheduleAtom);
@@ -51,14 +60,37 @@ export const GanttChartContent = () => {
                 <BasicButton onClick={onOpen}>ADD</BasicButton>
             </Box>
             {data?.length ? (
-                <Gantt
-                    tasks={data!}
-                    listCellWidth=""
-                    viewDate={viewDate}
-                    todayColor={blue[200]}
-                    barProgressColor={blue[600]}
-                    onClick={onOpenSchedule}
-                />
+                <>
+                    <RadioGroup row defaultValue="day">
+                        <FormControlLabel
+                            value="year"
+                            control={<Radio />}
+                            label="Year"
+                            onChange={() => setView(ViewMode.Year)}
+                        />
+                        <FormControlLabel
+                            value="month"
+                            control={<Radio />}
+                            label="Month"
+                            onChange={() => setView(ViewMode.Month)}
+                        />
+                        <FormControlLabel
+                            value="day"
+                            control={<Radio />}
+                            label="Day"
+                            onChange={() => setView(ViewMode.Day)}
+                        />
+                    </RadioGroup>
+                    <Gantt
+                        tasks={data!}
+                        listCellWidth=""
+                        viewDate={viewDate}
+                        todayColor={blue[200]}
+                        barProgressColor={blue[600]}
+                        onClick={onOpenSchedule}
+                        viewMode={view}
+                    />
+                </>
             ) : (
                 <Box sx={{ width: "100%", mt: "50px" }}>
                     <Typography
